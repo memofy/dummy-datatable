b3cac1fadfa219ea3785f5a23e121a0d2d9a4297
insanz01 <insankamil002@gmail.com>
2020-12-15 09:45:20 +0700

    second deploy

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <v-container fluid>
-      <v-layout justify-space-between row wrap>
+      <v-layout v-if="selected.length > 0" justify-space-between row wrap>
         <v-flex lg2>
-          <p>With Selected :</p>
+          <p @click="debugging">With Selected :</p>
         </v-flex>
         <v-flex lg2>
           <v-overflow-btn
@@ -24,52 +24,66 @@
         </v-flex>
         <v-flex></v-flex>
       </v-layout>
-      <v-data-table
-        v-model="selected"
-        :headers="contentHeaders"
-        :items="content"
-        :single-expand="singleExpand"
-        :expanded.sync="expanded"
-        @item-expanded="loadDetails"
-        :single-select="singleSelect"
-        show-select
-        item-key="name"
-        show-expand
-        class="elevation-1"
-      >
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>Expandable Table</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-switch
-              v-model="singleExpand"
-              label="Single expand"
-              class="mt-2"
-            ></v-switch>
-          </v-toolbar>
+      <v-card>
+        <v-card-title>
+          Tabel
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          v-model="selected"
+          :search="search"
+          :headers="contentHeaders"
+          :items="content"
+          :single-expand="singleExpand"
+          :expanded.sync="expanded"
+          @item-expanded="loadDetails"
+          :single-select="singleSelect"
+          show-select
+          item-key="name"
+          show-expand
+          class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Expandable Table</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-switch
+                v-model="singleExpand"
+                label="Single expand"
+                class="mt-2"
+              ></v-switch>
+            </v-toolbar>
 
-        </template>
-        <template v-slot:expanded-item="{ headers, item }">
-          <td :colspan="headers.length">
-           <v-simple-table>
-              <tbody>
-                <tr>
-                  <td>Logged :</td>
-                  <td><v-btn small rounded color="primary" dark>{{ item.summary.logged }}</v-btn></td>
-                </tr>
-                <tr>
-                  <td>State :</td>
-                  <td><v-btn small rounded color="primary" dark>{{ item.summary.state }}</v-btn></td>
-                </tr>
-                <tr>
-                  <td>Status :</td>
-                  <td><v-btn small rounded color="success" dark>{{ item.summary.status }}</v-btn></td>
-                </tr>
-              </tbody>
-           </v-simple-table>
-          </td>
-        </template>
-      </v-data-table>
+          </template>
+          <template v-slot:expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+             <v-simple-table>
+                <tbody>
+                  <tr>
+                    <td>Logged :</td>
+                    <td><v-btn small rounded color="primary" dark>{{ item.summary.logged }}</v-btn></td>
+                  </tr>
+                  <tr>
+                    <td>State :</td>
+                    <td><v-btn small rounded color="primary" dark>{{ item.summary.state }}</v-btn></td>
+                  </tr>
+                  <tr>
+                    <td>Status :</td>
+                    <td><v-btn small rounded color="success" dark>{{ item.summary.status }}</v-btn></td>
+                  </tr>
+                </tbody>
+             </v-simple-table>
+            </td>
+          </template>
+        </v-data-table>
+      </v-card>
     </v-container>
   </v-app>
 </template>
@@ -78,6 +92,7 @@
 /* eslint-disable */
 
 import Vue from 'vue';
+import axios from 'axios';
 
 declare interface Content {
   sn: string;
@@ -96,6 +111,7 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
+    search: '',
     expanded: [],
     selected: [] as Content[],
     statusActions: ['Update Status', 'Delete Data'],
@@ -124,15 +140,20 @@ export default Vue.extend({
   created() {
 
     const apiData: any[] = [
-      { sn: '55098127423490', model: 'SAM-SMR01', name: 'Smart Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
-      { sn: '55098127423491', model: 'SAM-SMR01', name: 'Silly Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
-      { sn: '55098127423492', model: 'SAM-SMR01', name: 'Genius Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
+      { uid: 1, sn: '55098127423490', model: 'SAM-SMR01', name: 'Smart Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
+      { uid: 2, sn: '55098127423491', model: 'SAM-SMR01', name: 'Silly Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
+      { uid: 3, sn: '55098127423492', model: 'SAM-SMR01', name: 'Genius Relay', mac: '', sammyVer: '1.0.0', firmwareVer: 'WDB01/1.00', registered: '11/17/2020, 2:07:49 PM', user: '', logLimit: '0 B', summary: ''},
     ];
 
     this.content = [...apiData];
   },
   methods: {
+    debugging() {
+      console.log(this.selected);
+    },
     loadDetails({item}: any) {
+
+      axios.get('https://jsonplaceholder.typicode.com/todos/'+item.uid).then(res => console.log(res.data));
 
       // get from http / api
       const APIConsume = {logged: 'Yes', state: 'Initializing', status: 'Activated'}
